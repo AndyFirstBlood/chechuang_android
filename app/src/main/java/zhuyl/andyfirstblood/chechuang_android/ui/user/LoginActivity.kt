7fb44adcafd23f812dda74c491b5f6ea5b9239ca
zhuyilin10 <yeying10@hotmail.com>
2016-12-24 22:53:49 +0800

    继续优化底层代码逻辑2

--- app/src/main/java/zhuyl/andyfirstblood/chechuang_android/ui/user/LoginActivity.kt
+++ app/src/main/java/zhuyl/andyfirstblood/chechuang_android/ui/user/LoginActivity.kt
@@ -1,8 +1,6 @@
 package zhuyl.andyfirstblood.chechuang_android.ui.user
 
 import android.os.Bundle
-import android.widget.ImageView
-import kotterknife.bindView
 import rx.android.schedulers.AndroidSchedulers.mainThread
 import zhuyl.andyfirstblood.chechuang_android.R
 import zhuyl.andyfirstblood.chechuang_android.client.SendSmsRequest
@@ -11,11 +9,9 @@ import zhuyl.andyfirstblood.chechuang_android.ui.BaseActivity
 
 @BindContentView(R.layout.activity_login)
 class LoginActivity : BaseActivity() {
-    val LoginImage: ImageView by bindView(R.id.aaa)
 
     override fun initContentView() {
         super.initContentView()
-
     }
     override fun initData(savedInstanceState: Bundle?) {
         super.initData(savedInstanceState)
